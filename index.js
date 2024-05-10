b43cff177eca4f30132cdcf08d9ccacd6c86ee6f
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2024-05-10 23:13:21 +0530

    Part5 - Filesystem module

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const _ = require("lodash");
+const fs = require("node:fs");
 
-let array_1 = [1, 2, 3];
-let array_2 = [2, 3, 4];
 
-console.log(_.intersection(array_1, array_2));
+fs.writeFile("/tmp/nodejspart5.txt", "Explaining node filesystem", "utf-8", function(err) {
+  if (err) {
+    console.log(err);
+    console.log("Failed to write");
+  } else {
+    console.log("Successfully Written!")
+  }
+});
+
+// fs.writeFileSync("/tmp/nodejspart5.txt", "Explaining filesystem module synchronous", "utf-8");
+
+// fs.readFile("./package.json", "utf-8", function(err, data) {
+//   if(err) {
+//     console.error(err);
+//     console.log("Readfile failed");
+//     return;
+//   }
+//   console.log(data);
+
+// })
