3feb7152e496acf4a63b2924ae053a5d490f22b6
Raj Bharath AgileLab <raj.bharath@agilelab.it>
2024-05-13 17:26:37 +0530

    Part8

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-const _ = require("lodash");
+const http = require('node:http');
 
-let array_1 = [1, 2, 3];
-let array_2 = [2, 3, 4];
+function homePageResponse() {
+  return "<html><head><title>Home Page</title></head><body>I am in Home page</body></html>"
+}
 
-console.log(_.intersection(array_1, array_2));
+function contactPageResponse() {
+  return "<html><head><title>Contact Page</title></head><body>Contact @ thazhal software</body></html>"
+}
+
+function responseErrHandler(err) {
+  if (err) {
+    console.error(err);
+    console.log("Error in sending response");
+    return;
+  }
+}
+const server = http.createServer(function (req, res) {
+  console.log("Request Received");
+  console.log(req.url);
+  console.log(req.method);
+  if (req.method == 'GET' && req.url == "/home") {
+    res.statusCode = 200
+
+    res.write(homePageResponse(), responseErrHandler);
+  } if (req.method == 'GET' && req.url == "/contact") {
+    res.statusCode = 200
+
+    res.write(contactPageResponse(), responseErrHandler);
+  } else {
+    res.statusCode = 400
+    res.statusMessage = "Server only supports GET request.";
+  }
+  res.end();
+});
+
+server.listen(8000);
